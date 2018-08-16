93ec8ef3ac57dc7997b7262040a798ba9a7fae07
Javi <arancibiajav@gmail.com>
2018-08-16 16:30:02 +0200

    rpc auth with privateKey

--- app/functions/checkAuth.js
+++ app/functions/checkAuth.js
@@ -1,17 +1,32 @@
 module.exports = app => roles => {
 	return function(req, res, next) {
-		if (req.user) {
-			if (roles instanceof Array) {
-				if (!roles.includes(req.user.type)) {
-					return res.status(401).send();
+		
+		app.fn.parseJson(req, res, async () => {
+
+			let privateKey = req.body.privateKey
+			if(privateKey){
+				let pr = await app.model('project').findOne({
+					privateKey
+				}).exec();
+				if(pr){
+					return next()
 				}
 			}
-			if (typeof role === 'string' && role !== req.user.type) {
-				return res.status(401).send();
+
+			if (req.user) {
+				if (roles instanceof Array) {
+					if (!roles.includes(req.user.type)) {
+						return res.status(401).send();
+					}
+				}
+				if (typeof role === 'string' && role !== req.user.type) {
+					return res.status(401).send();
+				}
+				next();
+			} else {
+				return res.status(401).send()
 			}
-			next();
-		} else {
-			return res.status(401).send()
-		}
+
+		})
 	}
 }
